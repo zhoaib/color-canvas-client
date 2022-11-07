41cbeb167f2fa26249ffd45a76b2976c7d09e2c5
zhoaib <zhoaibhassan@gmail.com>
2022-11-08 04:55:16 +0600

    banner

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
     </>
 
     return (
-        <div className="navbar h-20 p-10 ">
+        <div className="navbar h-20 p-10  bg-black">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
