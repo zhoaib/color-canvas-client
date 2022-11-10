ac59a8b6565ece0a188f13491dd80aeafd9bca90
zhoaib <zhoaibhassan@gmail.com>
2022-11-10 16:14:45 +0600

    localstorage

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -37,6 +37,7 @@ const Login = () => {
                     .then(res => res.json())
                     .then(data => {
                         console.log(data);
+                        localStorage.setItem('token', data.token);
                     })
 
                 // navigate(from, { replace: true });
