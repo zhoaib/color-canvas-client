d3c7dab82c6f0f962270af2adaadf15fbc40591d
zhoaib <zhoaibhassan@gmail.com>
2022-12-21 20:33:37 +0600

    check2

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -13,7 +13,7 @@ const Services = () => {
         <div className='bg-base-200 py-10 my-5 rounded-lg'>
             <div className='text-center mb-5'>
                 <p className='text-2xl '>Services</p>
-                <p className='text-4xl'>Packages You Can Choose</p>
+                <p className='text-4xl'>Packages You Can Choose!</p>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 ml-5'>
                 {
